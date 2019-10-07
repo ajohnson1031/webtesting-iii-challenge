a1ac4e29f4c8a91500670d604fd869dcc8e1a90e
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-10-07 19:00:53 -0400

    added gate click testing

--- src/helpers/index.test.js
+++ src/helpers/index.test.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Display from "../display/Display";
-import { render } from "@testing-library/react";
+import Dashboard from "../dashboard/Dashboard";
 
-describe("Gate initial state.", () => {
+import { render, fireEvent } from "@testing-library/react";
+
+describe("Gate initial state", () => {
   it("defaults to unlocked and open", () => {
-    const { getByText, queryByText } = render(<Display />);
+    const { queryByText } = render(<Display />);
 
     expect(queryByText(/Unlocked/i)).toBeTruthy();
     expect(queryByText(/Open/i)).toBeTruthy();
+  });
+});
+
+describe("Gate standard functionality", () => {
+  it("cannot be closed or opened if it is locked", () => {
+    const { findByText, getByText } = render(<Dashboard />);
+    fireEvent.click(getByText(/close gate/i));
+    expect(getByText(/closed/i)).toBeTruthy();
+
+    fireEvent.click(getByText(/lock gate/i));
+    expect(getByText(/locked/i)).toBeTruthy();
+
+    fireEvent.click(getByText(/open gate/i));
+    expect(getByText(/locked/i) && getByText(/closed/i)).toBeTruthy();
   });
 });
