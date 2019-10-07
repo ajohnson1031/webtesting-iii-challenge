a5580e5e3011ad621d75949e6d048f6383f2e0ed
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-10-07 08:19:36 -0400

    added snapshots

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import Display from "./Display";
+import renderer from "react-test-renderer";
+import { animationFrame } from "rxjs/internal/scheduler/animationFrame";
+
+describe("<Display />", () => {
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Display />);
+
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
