4e64fcf767cc5ea05107e69d4216693e9236d440
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-10-07 20:40:29 -0400

    dashboard tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -2,11 +2,19 @@
 import React from "react";
 import Dashboard from "./Dashboard";
 import renderer from "react-test-renderer";
+import { render } from "@testing-library/react";
 
 describe("<Dashboard />", () => {
   it("matches snapshot", () => {
     const tree = renderer.create(<Dashboard />);
-
     expect(tree.toJSON()).toMatchSnapshot();
+  });
+
+  it("shows controls and display", () => {
+    const dash = render(<Dashboard />);
+    const display = document.getElementsByClassName("display");
+    const controls = document.getElementsByClassName("controls");
+    expect(display).toBeTruthy();
+    expect(controls).toBeTruthy();
   });
 });
